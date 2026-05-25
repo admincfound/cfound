3572bab173990eff4e716e9a5d2e432d1a1d2756
admincfound <admin.cfound@gmail.com>
2026-05-25 13:11:14 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'motion/react';
+import { motion } from 'framer-motion';
 import { signInWithPopup } from 'firebase/auth';
 import { auth, googleProvider, db } from '../lib/firebase';
 import { useNavigate, useLocation } from 'react-router-dom';
