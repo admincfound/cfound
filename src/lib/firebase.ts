403ac2ded2ee434e138e8317fc8f18b8d5f5ab45
admincfound <admin.cfound@gmail.com>
2026-05-25 14:12:19 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 import firebaseConfig from '../../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+// CONNECT TO YOUR NAMED FIRESTORE DATABASE
+export const db = initializeFirestore(
+  app,
+  {},
+  'ai-studio-24810410-2d44-4bca-af7d-11572783e2b5'
+);
 
 export const auth = getAuth(app);
 export const storage = getStorage(app);
