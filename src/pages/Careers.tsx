be0101053ed7260020134681577231d438e181ff
admincfound <admin.cfound@gmail.com>
2026-05-29 12:50:38 +0530

    Update Careers.tsx

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -164,7 +164,10 @@ export default function Careers() {
         }
       );
 
-      console.log("APPLICATION COUNT UPDATED", opp.id);
+        console.log("APPLICATION UPDATED", opp.id);
+      } catch (err) {
+        console.error("APPLICATION UPDATE FAILED", err);
+      }
       
       setUserApplications(prev => new Set(prev).add(opp.id));
 
