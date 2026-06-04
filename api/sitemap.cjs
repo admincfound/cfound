ee7ebe3f6a718adc13570d7ba1d1132ed4dc9c97
admincfound <admin.cfound@gmail.com>
2026-06-04 16:43:52 +0530

    Update sitemap.cjs

--- api/sitemap.cjs
+++ api/sitemap.cjs
@@ -1,6 +1,7 @@
 import admin from "firebase-admin";
 import serviceAccount from "../serviceAccountKey.json" with { type: "json" };
 
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
