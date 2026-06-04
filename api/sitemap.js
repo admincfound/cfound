6a940a0e45d4a6d6c777a09ddee67d627b423512
admincfound <admin.cfound@gmail.com>
2026-06-04 17:09:14 +0530

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -2,10 +2,10 @@ import admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        credential: admin.credential.cert({
+      projectId: process.env.project_id,
+      clientEmail: process.env.client_email,
+      privateKey: process.env.private_key?.replace(/\\n/g, "\n"),
     }),
   });
 }
