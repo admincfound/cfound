3a03f25a42149688c8469eed7afcbb9f4374b532
admincfound <admin.cfound@gmail.com>
2026-06-05 11:00:56 +0530

    Update job.js

--- api/job.js
+++ api/job.js
@@ -13,5 +13,63 @@ if (!admin.apps.length) {
 const db = admin.firestore();
 
 export default async function handler(req, res) {
-  res.status(200).send("API working");
+  try {
+    const slug = req.url.split("/").pop();
+    const id = slug.split("-").pop();
+
+    const snap = await db.collection("careers").doc(id).get();
+
+    if (!snap.exists) {
+      return res.status(404).send("Job not found");
+    }
+
+    const job = snap.data();
+
+    const schema = {
+      "@context": "https://schema.org",
+      "@type": "JobPosting",
+      title: job.title,
+      description: job.description,
+      datePosted: job.createdAt?.toDate?.()?.toISOString(),
+      validThrough: job.deadline,
+      employmentType: "FULL_TIME",
+      hiringOrganization: {
+        "@type": "Organization",
+        name: job.companyName,
+      },
+      jobLocationType: "TELECOMMUTE",
+      baseSalary: {
+        "@type": "MonetaryAmount",
+        currency: "INR",
+        value: {
+          "@type": "QuantitativeValue",
+          minValue: Number(job.minAmount),
+          maxValue: Number(job.maxAmount),
+          unitText: "MONTH"
+        }
+      }
+    };
+
+    res.setHeader("Content-Type", "text/html");
+
+    return res.send(`
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>${job.title}</title>
+        <script type="application/ld+json">
+        ${JSON.stringify(schema)}
+        </script>
+      </head>
+      <body>
+        <script>
+          window.location.href="/careers/${slug}";
+        </script>
+      </body>
+      </html>
+    `);
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
