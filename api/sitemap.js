623b29e8b59f240ad035edc29d164e4a7e7ac056
admincfound <admin.cfound@gmail.com>
2026-06-09 14:45:13 +0530

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,61 +1,17 @@
-import { initializeApp, cert, getApps } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-
-if (!getApps().length) {
-  initializeApp({
-    credential: cert({
-      projectId: process.env.project_id,
-      clientEmail: process.env.client_email,
-      privateKey: process.env.private_key?.replace(/\\n/g, "\n"),
-    }),
-  });
-}
-
-const db = getFirestore();
+import { adminDb } from "../firebase-admin.js";
 
 export default async function handler(req, res) {
   try {
-    const baseUrl = "https://www.cfound.in";
+    const test = await adminDb.listCollections();
 
-    const careersSnap = await db.collection("careers").get();
-
-    let urls = [
-      "/",
-      "/about",
-      "/services",
-      "/projects",
-      "/internship",
-      "/careers",
-      "/courses",
-      "/contact",
-    ];
-
-    careersSnap.forEach((doc) => {
-      const job = doc.data();
-
-      const slug =
-        job.slug ||
-        job.title
-          ?.toLowerCase()
-          .replace(/[^a-z0-9\\s-]/g, "")
-          .replace(/\\s+/g, "-");
-
-      urls.push(`/careers/${slug}-${doc.id}`);
+    res.status(200).json({
+      collections: test.map(c => c.id)
     });
-
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map((u) => `<url><loc>${baseUrl}${u}</loc></url>`).join("\n")}
-</urlset>`;
-
-    res.setHeader("Content-Type", "application/xml");
-    res.status(200).send(xml);
   } catch (err) {
-    console.error("SITEMAP ERROR:", err);
-    res.status(500).send(JSON.stringify({
+    res.status(500).json({
       message: err.message,
       code: err.code,
       stack: err.stack
-    }));
+    });
   }
 }
