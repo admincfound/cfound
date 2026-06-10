b4cd7580c8c0a119369ca403b1708a8a63d5c49e
admincfound <admin.cfound@gmail.com>
2026-06-10 14:20:51 +0530

    Update AdminJobForm.tsx

--- src/pages/AdminJobForm.tsx
+++ src/pages/AdminJobForm.tsx
@@ -385,7 +385,7 @@ export default function AdminCreateJob() {
 
           applications: 0,
           views: 0,
-          createdAt: serverTimestamp()
+          createdAt: serverTimestamp(),
           updatedAt: serverTimestamp()
         }
       );
